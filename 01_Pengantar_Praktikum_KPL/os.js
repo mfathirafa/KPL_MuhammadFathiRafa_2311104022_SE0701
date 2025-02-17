18caea200585c361f8d7c4e931ad3b9ca58a4747
Muhammad Fathi Rafa <mfathirafa@gmail.com>
2025-02-17 16:10:55 +0700

    Commit ke-2

---
+++ 01_Pengantar_Praktikum_KPL/os.js
@@ -0,0 +1,9 @@
+import os from "os";
+//biar tahu isi laptop
+console.info(os.platform());
+console.info(os.arch());
+console.info(os.cpus());
+console.info(os.uptime());
+console.info(os.totalmem());
+console.info(os.freemem());
+console.info(os.networkInterfaces());
