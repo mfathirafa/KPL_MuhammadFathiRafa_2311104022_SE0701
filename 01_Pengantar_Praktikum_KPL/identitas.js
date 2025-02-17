3c24b385c0d1c8a0b6043f5a573a0509800c0c0c
Muhammad Fathi Rafa <mfathirafa@gmail.com>
2025-02-17 16:30:09 +0700

    commit ke-3

--- 01_Pengantar_Praktikum_KPL/identitas.js
+++ 01_Pengantar_Praktikum_KPL/identitas.js
@@ -8,6 +8,14 @@ const input = readline.createInterface({
 
 input.question("siapa nama kamu?", (nama) => {
     console.info(`Hallo ${nama}`)
-
-    input.close();
+    input.question("jurusan kamu?", (jurusan) => {
+        console.info(`Hallo ${jurusan}`)
+        input.question("dari univ mana kamu?", (universitas) => {
+            console.info(`Hallo ${universitas}`)
+            input.question("umur?", (umur) => {
+                console.info(` ${umur}`)
+                input.close();
+            })      
+        })        
+    })
 })
