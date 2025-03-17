a3d3fa419f88b2fda6e10e9237bc28edd3148d1b
mfathirafa <mfathirafa@gmail.com>
2025-03-17 11:38:32 +0700

    perbaikan utils.js

--- 05_Generics_DBC/utils.js
+++ 05_Generics_DBC/utils.js
@@ -2,7 +2,7 @@
 // Digunakan untuk memastikan kondisi tertentu sebelum eksekusi kode
 function assert(condition, message) {
     if (!condition) {
-        throw new Error(Assertion failed: ${message});
+        throw new Error(`Assertion failed: ${message}`);
     }
 }
 
@@ -10,7 +10,7 @@ function assert(condition, message) {
 // Fungsi ini memastikan bahwa nilai yang diterima adalah angka valid
 function ensureNumber(value, variableName) {
     if (typeof value !== "number" || isNaN(value)) {
-        throw new Error(Type Error: ${variableName} harus berupa angka yang valid.);
+        throw new Error(`Type Error: ${variableName} harus berupa angka yang valid.`);
     }
     return value;
 }
